a2d6a78d3b1f830683da296091a77441ba6b6f51
Vitalii Koreniev <nemish94@gmail.com>
2022-01-29 23:18:18 +0200

    Create keys for secrets, configure user for nginx and php-fpm, install packages for testing, update logging configuration

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
 ];
